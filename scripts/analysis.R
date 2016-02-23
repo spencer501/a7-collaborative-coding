d7ccf46ded6937989213687bccf688d037275fdf
Spencer <spencer.p.816@gmail.com>
2016-02-22 23:50:31 -0800

    Complete reformatting of input data.
    This is not an ideal solution because of issue #6, but it works.

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -1,1 +1,101 @@
 # Calculates summary information to be included in report
+
+### TODO: Wrap in function ###
+
+# needed libraries
+library(dplyr)
+
+data <- read.csv("https://raw.githubusercontent.com/INFO-498F/a7-survey-data/master/intro_survey_data.csv")
+
+# List to return
+ret <- list()
+
+# Find dimensions of data
+ret$length <- ncol(data)
+ret$observations <- nrow(data)
+
+# Equivalency lists:
+## - Create tables to convert responses from the original data frame to responses that are more succinct / numerically
+##   significant
+equiv <- list()
+
+equiv$scale3 <- data.frame(
+   'original' = c("Never used it",
+                  "Have used it a few times",
+                  "Intermediate user"),
+   'new' = c(1:3)
+)
+
+equiv$programming <-
+   data.frame(
+      'original' = c(
+         "Never written code",
+         "Experimented a bit with simple programming",
+         "Moderate experience with a scripting language (Python, R, JavaScript, Java, etc.)",
+         "Lots of experience with a scripting language (Python, R, JavaScript, Java, etc.)"
+      ),
+      'new' = c(1:4)
+   )
+
+equiv$pet <- data.frame(
+   'original' = c("A dog person...",
+                  "A cat person....",
+                  "Both!",
+                  "Neither"),
+   'new' = c("dog",
+             "cat",
+             "both",
+             "neither")
+)
+
+equiv$seahawks <- data.frame('original' = c("No",
+                                            "Yes",
+                                            "YES!"),
+                             'new' = c(1:3))
+
+                                     
+# Format data:
+## - Renames all the columns to more succinct names
+## - Change responses that are part of a scale (ex: experiece with git, command line, r, ect.) to have actual numeric
+##   value
+f_data <- data %>%
+   rename(
+      "standing" = What.is.your.current.class.standing.,
+      "applying" = Are.you.interested.in.applying.to.the.Informatics.major.,
+      "os" = What.operating.system.do.you.typically.use.,
+      "command_line" = Using.the.command.line...terminal,
+      "git" = What.is.your.familiarity.with..using.git.for.version.control,
+      "markdown" = What.is.your.familiarity.with..Creating.documents.with.Markdown,
+      "r_language" = What.is.your.familiarity.with..Using.the.R.programming.language,
+      "programming" = What.is.your.programming.experience.,
+      "countries" = How.many.countries.have.you.visited.in.your.life.,
+      "cat_v_dog" = Do.you.consider.yourself.,
+      "seahawks" = Are.you.a.Seahawks.fan.
+   )
+
+#### NOTE: This is the desired functionality. However, do to what I believe is an error in 'dplyr'
+#### (found at https://github.com/hadley/dplyr/issues/1400), this produces the error 'invalid subscript type 'closure''.
+#### For now, even though it is not the best practice, these mutations will be run line by line.
+# f_data <- f_data %>%
+#    mutate(command_line = equiv$scale3$new[match(f_data$command_line, equiv$scale3$original)],
+#           git = equiv$scale3$new[match(f_data$git, equiv$scale3$original)],
+#           markdown = equiv$scale3$new[match(f_data$markdown, equiv$scale3$original)],
+#           r_language = equiv$scale3$new[match(f_data$r_language, equiv$scale3$original)],
+#           programming = equiv$programming$new[match(f_data$programming, equiv$programming$original)],
+#           cat_v_dog = equiv$pet$new[match(f_data$cat_v_dog, equiv$pet$original)],
+#           seahawks = equiv$seahawks$new[match(f_data$seahawks, equiv$seahawks$original)])
+
+f_data$command_line = equiv$scale3$new[match(f_data$command_line, equiv$scale3$original)]
+f_data$git = equiv$scale3$new[match(f_data$git, equiv$scale3$original)]
+f_data$markdown = equiv$scale3$new[match(f_data$markdown, equiv$scale3$original)]
+f_data$r_language = equiv$scale3$new[match(f_data$r_language, equiv$scale3$original)]
+f_data$programming = equiv$programming$new[match(f_data$programming, equiv$programming$original)]
+f_data$cat_v_dog = equiv$pet$new[match(f_data$cat_v_dog, equiv$pet$original)]
+f_data$seahawks = equiv$seahawks$new[match(f_data$seahawks, equiv$seahawks$original)]
+
+
+
+
+
+
+
