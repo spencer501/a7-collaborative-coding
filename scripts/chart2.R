60268eba03245c3fd9ebcdfd4260ebb0f78300a2
Jared Trelles <jaredt5@uw.edu>
2016-02-23 16:33:54 -0800

    modified chart2.R. close #3

--- scripts/chart2.R
+++ scripts/chart2.R
@@ -1,1 +1,27 @@
 # Function that returns the second chart
+library(plotly)
+library(dplyr)
+
+build_second_chart <- function(dataset) {
+  freshman  <- dataset %>% filter(What.is.your.current.class.standing. == "Freshman")
+  sophomore <- dataset %>% filter(What.is.your.current.class.standing. == "Sophomore")
+  junior    <- dataset %>% filter(What.is.your.current.class.standing. == "Junior")
+  senior    <- dataset %>% filter(What.is.your.current.class.standing. == "Senior")
+  
+  chart <- plot_ly(y = freshman$How.many.countries.have.you.visited.in.your.life., 
+                   type = "box", 
+                   name = "Freshmen") %>%
+    add_trace(y = sophomore$How.many.countries.have.you.visited.in.your.life., 
+              type = "box", 
+              name = "Sophomores") %>%
+    add_trace(y = junior$How.many.countries.have.you.visited.in.your.life., 
+              type = "box", 
+              name = "Juniors") %>%
+    add_trace(y = senior$How.many.countries.have.you.visited.in.your.life., 
+              type = "box", 
+              name = "Seniors") %>%
+    layout(yaxis = list(title = "Countries Visited")
+    )
+  
+  return(chart)
+}
