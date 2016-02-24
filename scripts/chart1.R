b0374ebe9db14e186c87945235e27c4ceda6ca47
fionc <fionc@uw.edu>
2016-02-23 20:10:13 -0800

    finished chart one

--- scripts/chart1.R
+++ scripts/chart1.R
@@ -1,19 +1,27 @@
 # Function that returns the first chart\# Function that returns the first chart
 library(plotly)
+library(dplyr)
 
-build_chart <- function(data){
-  chart1 <- plot_ly( 
-                    x = class_standing$r_language, 
-                    y = n, 
-                    name = "Freshman",
-                    type = "bar",
-                    )
-  return(chart1)
+build_chart <- function(chart_one) {
+  freshman  <- chart_one %>% filter(standing == "Freshman")
+  sophomore <- chart_one %>% filter(standing == "Sophomore")
+  junior    <- chart_one %>% filter(standing == "Junior")
+  senior    <- chart_one %>% filter(standing == "Senior")
+  
+  chart <- plot_ly(y = freshman$`R Experience`, 
+                   type = "box", 
+                   name = "Freshmen") %>%
+    add_trace(y = sophomore$`R Experience`, 
+              type = "box", 
+              name = "Sophomores") %>%
+    add_trace(y = junior$`R Experience`, 
+              type = "box", 
+              name = "Juniors") %>%
+    add_trace(y = senior$`R Experience`, 
+              type = "box", 
+              name = "Seniors") %>%
+    layout(yaxis = list(title = "R Language Experience")
+    )
+  
+  return(chart)
 }
-
-
-#counts <- table(mtcars$vs, mtcars$gear)
-#barplot(counts, main="Car Distribution by Gears and VS",
-        #xlab="Number of Gears", col=c("darkblue","red"),
-        #legend = rownames(counts))
-
