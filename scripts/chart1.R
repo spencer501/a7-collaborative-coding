561b105c6adefd2ac13e08f2310a5d7ca44c1d50
fionc <fionc@uw.edu>
2016-02-23 19:00:11 -0800

    started chart

--- scripts/chart1.R
+++ scripts/chart1.R
@@ -1,1 +1,20 @@
-# Function that returns the first chart
+# Function that returns the first chart\# Function that returns the first chart
+library(plotly)
+
+build_chart <- function(dataf){
+  chart1 <- plot_ly(data = info$dataf, 
+                    x = info$dataf$command_line, 
+                    y = info$dataf$r_language, 
+                    mode = "markers",
+                    color = info$dataf$standing,
+                    colors = 
+                    )
+  return(chart1)
+}
+
+chart2 <- function (dataf){
+  charts <- info$data %>% 
+            count(r_language, standing) %>%
+  plot_ly(x = r_language, y = n, type = "bar", color = info$dataf$standing)
+  return(charts)
+}
