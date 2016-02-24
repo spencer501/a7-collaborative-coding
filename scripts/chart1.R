aa6714065c7c56a242aa7ce5273e0b18c2a78371
fionc <fionc@uw.edu>
2016-02-23 19:45:16 -0800

    continuing chart 1

--- scripts/chart1.R
+++ scripts/chart1.R
@@ -2,26 +2,18 @@
 library(plotly)
 
 build_chart <- function(data){
-  chart1 <- plot_ly(data = info$dataf, 
-                    x = info$dataf$command_line, 
-                    y = info$dataf$r_language, 
-                    mode = "markers",
-                    color = info$dataf$standing,
-                    colors = 
+  chart1 <- plot_ly( 
+                    x = class_standing$r_language, 
+                    y = n, 
+                    name = "Freshman",
+                    type = "bar",
                     )
   return(chart1)
 }
 
 
-# Stacked Bar Plot with Colors and Legend
 #counts <- table(mtcars$vs, mtcars$gear)
 #barplot(counts, main="Car Distribution by Gears and VS",
         #xlab="Number of Gears", col=c("darkblue","red"),
         #legend = rownames(counts))
 
-chart2 <- function (dataf){
-  charts <- info$data %>% 
-            count(r_language, standing) %>%
-  plot_ly(x = r_language, y = n, type = "bar", color = info$dataf$standing)
-  return(charts)
-}
